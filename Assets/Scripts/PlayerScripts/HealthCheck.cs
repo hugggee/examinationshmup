0f4f5896c2a49ed38f3e8d3f40a0dac75c37db42
Hugo Larsson <hugo.larsson1@learnet.se>
2024-01-10 12:29:00 +0100

    new verison

--- Assets/Scripts/PlayerScripts/HealthCheck.cs
+++ Assets/Scripts/PlayerScripts/HealthCheck.cs
@@ -3,15 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class HealthCheck : MonoBehaviour
 {
-    public Playerdata CurrentPlayerData = null; // Referens till din PlayerData-klass, du kan dra och släppa din spelare på detta fält i Unity-inspektören.
+    public Playerdata CurrentPlayerData = null;
 
     private void Update()
     {
         // Kontrollera spelarens hälsa
-        if (CurrentPlayerData != null && CurrentPlayerData.HP <= 0)
+        if (CurrentPlayerData != null)
         {
-            // Om spelarens hälsa är 0 eller mindre, byt scenen till huvudmenyn
-            SceneManager.LoadScene("mainmenu");
+            if (CurrentPlayerData.HP <= 1)
+            {
+                // Om spelarens hälsa är 1 eller mindre, byt scenen till huvudmenyn
+                SceneManager.LoadScene("mainmenu");
+            }
+            else if (SceneManager.GetActiveScene().name == "mainmenu")
+            {
+                // Om spelaren är i huvudmenyn, återställ hälsan till 10
+                CurrentPlayerData.HP = 10;
+            }
         }
     }
 }
